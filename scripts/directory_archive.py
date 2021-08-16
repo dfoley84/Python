2364722917cb477cabde83543057cc60472a5d91
dfoley84 <83467582+dfoley84@users.noreply.github.com>
2021-08-16 17:05:17 +0100

    Update directory_archive.py

--- scripts/directory_archive.py
+++ scripts/directory_archive.py
@@ -1,8 +1,15 @@
 import shutil
 import datetime
+import zipfile
 
 def Directory_zip(dirpath):
     shutil.make_archive('archive_'+ datetime, 'zip', dirpath )
 
+#read zip files
+def read_zip(filename):
+    with zipfile.ZipFile(filename,'r') as archive:
+        print(archive.namelist())
+
 if __name__ == "__main__":
     Directory_zip('C:\\bin')
+    #read_zip('archive_<>.zip')
