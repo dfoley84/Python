990f7512ca7cf079024f207d4c3b4131405d308b
dfoley84 <83467582+dfoley84@users.noreply.github.com>
2021-10-12 12:26:29 +0100

    Update selenium.py

--- selenium.py
+++ selenium.py
@@ -1,32 +1,38 @@
+from logging import setLogRecordFactory
+import unittest
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 
-driver = webdriver.Chrome()
-  
-# get geeksforgeeks.org
-driver.get("")
-  
 
 
-# get element 
-action = ActionChains(driver)
-pageTitle = driver.title
+class GetLowBP(unittest.TestCase):
 
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        self.addCleanup(self.browser.quit)
+   
+    def test_lowbp(self):
+        action = ActionChains(self.browser)
+        self.browser.get("https://bloodpressure-ca-staging.azurewebsites.net")
+        Systolic = self.browser.find_element_by_id("BP_Systolic")
+        Systolic.clear()
+        action.click(on_element = Systolic)
+        action.send_keys(70)
+        Diastolic = self.browser.find_element_by_id("BP_Diastolic")
+        Diastolic.clear()
+        action.click(on_element = Diastolic)
+        action.send_keys(60)
+        action.perform()
+        SubmitButton = self.browser.find_element_by_xpath("//input[@value='Submit']")
+        SubmitButton.submit()
+        Message = self.browser.find_elements_by_tag_name("body")
 
-
-element = driver.find_element_by_name("BP.Systolic")
-element.clear()
-action.click(on_element = element)
-action.send_keys(150)
-
-element1 = driver.find_element_by_name("BP.Diastolic")
-element1.clear()
-action.click(on_element = element1)
-action.send_keys(60)
-action.perform()
-
-element2 = driver.find_element_by_xpath("//input[@value='Submit']")
-element2.submit()
-
-
+        # To Do 
+         ##Assert Not Working Correctly on the Unit Testing. 
+        self.assertSetEqual("Low Blood Pressure", Message)
+        
+ 
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
 
