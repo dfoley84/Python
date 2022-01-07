ad32b3f84772a1551796d1249e916d9a71cf191b
dfoley84 <83467582+dfoley84@users.noreply.github.com>
2022-01-07 12:58:36 +0000

    Create Flask_RabbitMQ.py

---
+++ scripts/Flask_RabbitMQ.py
@@ -0,0 +1,28 @@
+from flask import Flask
+import pika
+import json
+
+app = Flask(__name__)
+
+message = {'id': 1, 'name': 'name1'}
+
+@app.route('/')
+def hello_world():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=''))
+    channel = connection.channel()
+    channel.queue_declare(queue='Test', durable=False)
+    channel.basic_publish(
+        exchange='',
+        routing_key='Test',
+        body=json.dumps(message),
+        properties=pika.BasicProperties(
+            delivery_mode=2,
+        ))
+    connection.close()
+    return 'sent'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
+
