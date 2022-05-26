a34471cb0074cc0a27af3d287a12ac8d2bbed013
dfoley84 <83467582+dfoley84@users.noreply.github.com>
2022-05-26 09:37:52 +0100

    Create elastic_ipaddr.py

---
+++ boto3/elastic_ipaddr.py
@@ -0,0 +1,23 @@
+import boto3
+import hvac
+
+client = hvac.Client(url='http://VaultServer:8200')
+read_response = client.read('secret/aws/credentials')
+
+sec_key = read_response['data']['aws_secret_access_key']
+acc_key = read_response['data']['aws_access_key_id']
+
+
+session = boto3.Session(
+    aws_access_key_id=acc_key,
+    aws_secret_access_key=sec_key
+    region_name='eu-west-1'
+)
+
+EC2_client = session.client('ec2')
+
+response = EC2_client.describe_addresses()
+for address in response['Addresses']:
+    if address['InstanceId'] is None:
+        print(address['PublicIp'])
+        EC2_client.release_address(AllocationId=address['AllocationId'])
