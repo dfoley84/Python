6a3c7148a96adc38eb5ccfaa5a5134eecafbbeb1
dfoley84 <83467582+dfoley84@users.noreply.github.com>
2022-12-21 14:47:01 +0000

    Create s3_blockpublicaccess.py

---
+++ boto3/SecurityHub/s3_blockpublicaccess.py
@@ -0,0 +1,34 @@
+from urllib import response
+import boto3
+from botocore.exceptions import ClientError
+
+
+Session = boto3.Session()
+
+s3 = boto3.client('s3', region_name='eu-west-1')
+response = s3.list_buckets()
+
+for bucket in response['Buckets']:
+    try:
+        s3.put_public_access_block(
+                Bucket = bucket['Name'],
+                PublicAccessBlockConfiguration={
+                    'BlockPublicAcls': True,
+                    'IgnorePublicAcls': True,
+                    'BlockPublicPolicy': True,
+                    'RestrictPublicBuckets': True
+                }
+            )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'NoSuchPublicAccessBlockConfiguration':
+            print('Bucket: %s, no public access block' % (bucket['Name']))
+            s3.put_public_access_block(
+                Bucket = bucket['Name'],
+                PublicAccessBlockConfiguration={
+                    'BlockPublicAcls': True,
+                    'IgnorePublicAcls': True,
+                    'BlockPublicPolicy': True,
+                    'RestrictPublicBuckets': True
+                }
+            )
+
