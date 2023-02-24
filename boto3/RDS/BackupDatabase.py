b4d33b68379afe68840dbccbce76e749d5392f47
dfoley84 <83467582+dfoley84@users.noreply.github.com>
2023-02-24 23:53:12 +0000

    Create BackupDatabase.py

---
+++ boto3/RDS/BackupDatabase.py
@@ -0,0 +1,103 @@
+from sshtunnel import open_tunnel
+import paramiko
+import pymysql
+
+def SourceRDSInstance():    
+    command = [
+    'mysqldump --user="%s" --password="%s" -h %s ebdb > /tmp/%s.sql' % (SourceUser, Sourcepwd, SourceRDS, customer)
+    ]
+    try:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(REMOTE_HOST, username='ec2-user', key_filename='./t.pem')
+
+        for command in command:
+            print("running command: {}".format(command))
+            stdin , stdout, stderr = ssh.exec_command(command)
+            print(stdout.read())
+            print(stderr.read())
+        ssh.close()
+    except Exception as e:    
+        print(e)
+
+def DropTable():
+    print('Start Dropping Table')
+    #Drop ebdb table in destination RDS Instance
+    with open_tunnel(
+        (REMOTE_HOST, 22),
+        ssh_username='ec2-user',
+        ssh_pkey='./t.pem',
+        remote_bind_address=(TargetRDS, 3306)) as server:
+        conn = pymysql.connect(host='127.0.0.1', user=TargetUser, port=server.local_bind_port, passwd=Targetpwd)
+        cur = conn.cursor()
+        sql = [
+        "DROP DATABASE IF EXISTS ebdb",
+        "CREATE DATABASE ebdb"
+        ]
+        for sql in sql:
+            print("running command: {}".format(sql))
+            cur.execute(sql)
+            print(cur)
+        cur.close()
+    
+
+def RestoreDB():
+    print('Start Restoring')
+    #Restore ebdb table in destination RDS Instance
+    command = [
+    'mysql --user=%s --password="%s" -h %s ebdb < /tmp/%s.sql' % (TargetUser, Targetpwd, TargetRDS, customer)
+    ]
+    try:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(REMOTE_HOST, username='ec2-user', key_filename='./t.pem')
+
+        for command in command:
+            print("running command: {}".format(command))
+            stdin , stdout, stderr = ssh.exec_command(command)
+            print(stdout.read())
+            print(stderr.read())
+        ssh.close()
+
+    except Exception as e:    
+        print(e)
+
+
+#Remove SQLDump File From EC2 
+def RemoveDumpFile():
+    command = ['sudo rm -rf /tmp/%s.sql' % (customer) ]
+    try:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(REMOTE_HOST, username='ec2-user', key_filename='./t.pem')
+        
+        for command in command:
+            print("running command: {}".format(command))
+            stdin , stdout, stderr = ssh.exec_command(command)
+            print(stdout.read())
+            print(stderr.read())
+        ssh.close()
+    except Exception as e:    
+        print(e)
+
+
+if __name__ == '__main__':
+    #Input Parameters
+    SourceRDS = input('Source RDS Instance: ')
+    TargetRDS = input('Target RDS Instance: ')
+    SourceUser = input('Source RDS User: ')
+    TargetUser = input('Target RDS User: ')
+    Sourcepwd = input('Source RDS Password: ')
+    Targetpwd = input('Target RDS Password: ')
+    customer = input('Customer: ')
+    REMOTE_HOST = #EC2 Public DNS Name
+    
+    SourceRDSInstance()
+    DropTable()
+    RestoreDB()
+    RemoveDumpFile()
+
+
+
+
+
