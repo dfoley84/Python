417a41d150dc37b5e4231319e2f1733e239102e0
dfoley84 <83467582+dfoley84@users.noreply.github.com>
2023-08-02 21:45:22 +0100

    Update main.py

--- FastAPI/AWS/main.py
+++ FastAPI/AWS/main.py
@@ -3,25 +3,15 @@ import boto3
 import logging
 from EC2 import router as ec2_aws_router
 from RDS import router as rds_aws_router
+from S3 import router as s3_aws_router
 
 logging.basicConfig(level=logging.INFO)
 
 app = FastAPI()
 
-app.include_router(ec2_aws_router, prefix="/ec2")
-app.include_router(rds_aws_router, prefix="/rds")
+app.include_router(ec2_aws_router, prefix="/ec2", tags=["EC2"])
+app.include_router(rds_aws_router, prefix="/rds", tags=["RDS"])
+app.include_router(s3_aws_router, prefix="/s3", tags=["S3"])
 
-
-
-#Get All S3 Buckets
-@app.get("/s3/buckets",tags=["S3"])
-def get_all_buckets(region: str, aws_access_key_id: str, aws_secret_access_key: str, aws_session_token: str):
-    s3 = boto3.resource('s3',
-                        region_name=region,
-                        aws_access_key_id=aws_access_key_id,
-                        aws_secret_access_key=aws_secret_access_key,
-                        aws_session_token=aws_session_token)
-    buckets = s3.buckets.all()
-    return [bucket.name for bucket in buckets]
 
 
