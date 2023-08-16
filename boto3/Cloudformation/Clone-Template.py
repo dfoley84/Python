2a4872ae38d8d8f6e075a26c3fbbf465e55c2f23
dfoley84 <83467582+dfoley84@users.noreply.github.com>
2023-08-16 10:16:39 +0100

    Update Clone-Template.py

--- boto3/Cloudformation/Clone-Template.py
+++ boto3/Cloudformation/Clone-Template.py
@@ -2,33 +2,46 @@ import boto3
 boto3.Session()
 
 
-def getCFTemplate(SourceRegion, TargetRegion, StackName, Role):
+def getparameters(SourceRegion, StackName, updated_parameters=None):
+
+    SourceCF = boto3.client('cloudformation', region_name=SourceRegion)
+    response = SourceCF.describe_stacks(StackName=StackName)
+
+    parameters = [] #Empty List to hold Parameters
+
+    for parameter in response['Stacks'][0]['Parameters']:
+        key = parameter['ParameterKey']
+        if updated_parameters and key in updated_parameters: 
+            value = updated_parameters[key]
+        else:
+            value = parameter['ParameterValue']
+        parameters.append({'ParameterKey': key, 'ParameterValue': value})
+    return parameters
+
+
+def getCFTemplate(SourceRegion, TargetRegion, StackName, DRStackName, updated_parameters=None):
 
     SourceCF = boto3.client('cloudformation', region_name=SourceRegion)
     TargetCF = boto3.client('cloudformation', region_name=TargetRegion)
-  
+
+    parameters = getparameters(SourceRegion, StackName,updated_parameters) 
+
     try:
         response = SourceCF.get_template(StackName=StackName)
-        template = response['TemplateBody']
+        template = response['TemplateBody'] 
 
-        #Get Parameters from the source CF Template
-        response = SourceCF.describe_stacks(StackName=StackName)
-        parameters = response['Stacks'][0]['Parameters']
-        print(parameters)
-
-        #Create the Stack in different Region
-        StackClone = StackName + '-backup'
+        #Create a Clone of the Source Stack in the Target Region
         response = TargetCF.create_stack(
-            StackName=StackClone,
+            StackName=DRStackName,
             TemplateBody=template,
-            RoleARN=Role,
             Parameters=parameters,
+            #RoleARN=RoleName, 
             Capabilities=[
                 'CAPABILITY_NAMED_IAM',
                 'CAPABILITY_IAM'
                 ],
             )
-        
+        #Wait for the Stack to be created
         waiter = TargetCF.get_waiter('stack_create_complete')
         waiter.wait(StackName=StackName)
         print('Stack Created')
@@ -36,16 +49,18 @@ def getCFTemplate(SourceRegion, TargetRegion, StackName, Role):
     except Exception as e:
         print('Error: {}'.format(e))
         return False
-    
     return True
+
+
 if __name__ == "__main__":
-    Role = 'arn:aws:iam::<AccountID>:role/<ROLE>'
-    getCFTemplate('<REGION>', '<REGION>', '<CF-Name>', Role)
+    #RoleName = 'arn:aws:iam::ACCOUNT_ID:role/ROLE' <Change this to the ECS Role ARN> 
+
+
+    #Overwrite the Parameters As Needed.
+    updated_params = {
+    }
+
+    getCFTemplate('eu-west-1', 'eu-central-1', updated_parameters=updated_params)
+    
 
     
-         
-
-        
-
-
-
