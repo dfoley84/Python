e5e5ca45762db5fd067497b97849d7c44f1dd959
dfoley84 <83467582+dfoley84@users.noreply.github.com>
2025-01-16 09:51:10 +0000

    Create changeRDSEngineVersion.py

---
+++ boto3/RDS/changeRDSEngineVersion.py
@@ -0,0 +1,24 @@
+import boto3 
+import os
+
+region = ['eu-west-1','us-east-1','us-west-2','ap-southeast-2']
+
+def handler():
+    for i in region:
+        rds = boto3.client('rds', region_name=i)
+        #get All RDS instances
+        response = rds.describe_db_instances()
+        for instance in response['DBInstances']:
+            instance_name = instance['DBInstanceIdentifier']
+            instance_type = instance['DBInstanceClass']
+            instance_engine = instance['Engine']
+            EngineVersion = instance['EngineVersion']
+            if instance_engine == 'mysql' and EngineVersion == '8.0.36':
+                rds.modify_db_instance(
+                    DBInstanceIdentifier=instance_name, 
+                    EngineVersion='8.0.40',
+                    ApplyImmediately=True
+                )
+                print (f"Instance {instance_name} is being updated to MySQL 8.0.40")
+if __name__ == '__main__':
+    handler()
